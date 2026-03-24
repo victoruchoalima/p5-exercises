0a630f463a101f511face96fe1e7fac893736676
victor lima <victoruchoalima@gmail.com>
2026-03-23 21:35:18 -0300

    p5-exercises-010-2

--- 001/p5-exercises-010/sketch.js
+++ 001/p5-exercises-010/sketch.js
@@ -2,8 +2,8 @@ let bubble1, bubble2;
 
 function setup() {
     createCanvas(800, 600);
-    bubble1 = new Bubble();
-    bubble2 = new Bubble();
+    bubble1 = new Bubble(200,200,40);
+    bubble2 = new Bubble(400,200,20);
 }
 
 function draw() {
@@ -19,9 +19,10 @@ function draw() {
 
 
 class Bubble {
-    constructor(){
-        this.x=200;
-        this.y=150;
+    constructor(x,y,r){
+        this.x=x;
+        this.y=y;
+        this.r=r;
     }
 
     move(){
@@ -33,6 +34,6 @@ class Bubble {
         stroke(255)
         strokeWeight(4)
         noFill()
-        ellipse(this.x, this.y, 24, 24)
+        ellipse(this.x, this.y, this.r*2)
     }
 }
