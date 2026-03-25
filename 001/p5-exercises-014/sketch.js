3831109cd3bf778c744bfab5758ac1a60d71dbaf
victor lima <victoruchoalima@gmail.com>
2026-03-25 17:01:10 -0300

    p5-exercises-014-01/02

---
+++ 001/p5-exercises-014/sketch.js
@@ -0,0 +1,73 @@
+let bubbles=[]
+
+function setup(){
+    createCanvas(windowWidth,windowHeight)
+
+    for (let i=0; i<10;i++){
+        let x = random(width)
+        let y = random(height)
+        let r = random(20,60)
+        let b = new Bubble(x,y,r);
+        
+        bubbles.push(b)
+    }
+}
+
+function mousePressed(){
+    for (let b=bubbles.length-1;b>=0;b--){
+        if(bubbles[b].contains(mouseX,mouseY)){
+            bubbles.splice(b,1)
+        }
+    }
+}
+
+function draw(){
+    background(0)
+
+    for(let i=0; i<bubbles.length; i++){
+        if (bubbles[i].contains(mouseX,mouseY)){
+            bubbles[i].changeColor(255)
+        } else {
+            bubbles[i].changeColor(0)
+        }
+
+        bubbles[i].move()
+        bubbles[i].show()
+    }
+
+}
+
+class Bubble{
+    constructor(x,y,r){
+        this.x=x
+        this.y=y
+        this.r=r
+        this.brightness=0
+    }
+
+    changeColor(bright){
+        this.brightness=bright
+    }
+
+    contains(px,py){
+        let d = dist(px,py,this.x,this.y)
+
+        if (d<this.r){
+            return true
+        } else {
+            return false
+        }
+    }
+
+    move(){
+        this.x = this.x + random(-2,2)
+        this.y = this.y + random(-2,2)
+    }
+
+    show(){
+        stroke(255)
+        strokeWeight(4)
+        fill(this.brightness,124)
+        ellipse(this.x,this.y,this.r*2)
+    }
+}
